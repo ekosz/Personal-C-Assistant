ea4dbbe79d4c20a3ea659c1946474356748e9522
Eric Koslow <ekoslow@gmail.com>
2011-03-14 18:44:32 -0400

    Potental Fix to get_name

--- lib/twilio_helpers.rb
+++ lib/twilio_helpers.rb
@@ -1,6 +1,6 @@
 module Sinatra::TwilioHelpers
   def get_name(number)
-    Main::REDIS.keys.each do |key|
+    Main::REDIS.keys.select {|s| s=~ /number/}.each do |key|
       num = JSON.parse(Main::REDIS.get(key))
       if num['number'] == number
         return num['name']
