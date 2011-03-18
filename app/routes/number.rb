db81c22d4bd6fbfaa9d46896a79b6c03c6a4980c
Eric Koslow <ekoslow@gmail.com>
2011-03-18 10:39:16 -0400

    Added more styling.  Protected new number routes from non-logged in Members.

--- app/routes/number.rb
+++ app/routes/number.rb
@@ -1,20 +1,24 @@
 class Main < Sinatra::Base
   get '/numbers' do
+    ensure_authenticated(User)
     @numbers = Number.all
     haml :"numbers/index"
   end
 
   get '/numbers/new' do
+    ensure_authenticated(User)
     @number = Number.new
     haml :"numbers/new"
   end
 
   get '/numbers/:id' do
+    ensure_authenticated(User)
     @number = Number[params[:id]]
     haml :"numbers/show"
   end
 
   get '/numbers/:id/edit' do
+    ensure_authenticated(User)
     @number = Number[params[:id]]
     haml :"numbers/edit"
   end
